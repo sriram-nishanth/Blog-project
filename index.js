d1439952c758e39445f745a60d62a8f4071bd6f4
sriram-nishanth <sriramnishanth2003@gmail.com>
2025-04-10 18:23:46 +0530

    first-version

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const posts = [];
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{posts})
+});
+
+app.post("/create", (req, res) => {
+    const newPost = {
+        id: Date.now().toString(),
+        title: req.body.title,
+        content: req.body.content
+    };
+    posts.push(newPost);
+    console.log("Updated Posts Array:", posts);
+    res.redirect("/");
+});
+
+
+app.get("/edit/:id", (req, res) => {
+    const post = posts.find(p => p.id === req.params.id);
+    
+    if (!post) {
+        console.log("Post not found");
+        return res.redirect("/");
+    }
+
+    console.log("Loading Edit Page for:", post);
+    res.render("edit.ejs", { post });
+});
+
+app.post("/update/:id", (req, res) => {
+    const postIndex = posts.findIndex(p => p.id === req.params.id);
+    if (postIndex !== -1) {
+        posts[postIndex].title = req.body.title;
+        posts[postIndex].content = req.body.content;
+    }
+    res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+    const index = posts.findIndex(p => p.id === req.params.id);
+    if (index !== -1) posts.splice(index, 1);
+    res.redirect("/");
+});
+
+app.listen(port,()=>{
+    console.log(`server running ${port}`);
+});
