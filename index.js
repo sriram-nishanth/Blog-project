4c888d5134c70355f5b4591ad33b63867662430c
sriram-nishanth <sriramnishanth2003@gmail.com>
2025-05-15 19:21:28 +0530

    commited

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
