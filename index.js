ffc584c7573720b3c5201acbf54c5df934536738
sriram-nishanth <sriramnishanth2003@gmail.com>
2025-05-15 19:40:15 +0530

    changes

--- index.js
+++ index.js
@@ -56,7 +56,9 @@ app.post("/delete/:id", (req, res) => {
     res.redirect("/");
 });
 
-// Remove or comment out this line for Vercel:
-//app.listen(3000, () => console.log('Server running'));
+const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 export default app;
